a00483884fb079d55a7f1888de7ba0d148dcbf1e
Nikita <lnikbzr@gmail.com>
2025-01-22 10:26:59 +0500

    Lab2

---
+++ Lab2/add_sets.py
@@ -0,0 +1,11 @@
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
---
+++ Lab2/sort_list.py
@@ -0,0 +1,6 @@
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+thislist.sort(reverse = True)
+print(thislist)
