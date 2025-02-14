4ba2d07ce96d46f93007778963ec5461b625186c
Nikita <lnikbzr@gmail.com>
2025-02-15 01:39:50 +0500

    Lab 4 mod

--- Lab4/json/1.py
+++ Lab4/json/1.py
@@ -16,14 +16,19 @@ mtu_width = 10
 print("Interface Status")
 print("="*(sum(headers.values())))
 
-print(f"{'DN'.ljust(dn_width)}{'Description'.ljust(des_width)}{'Speed'.ljust(speed_width)}{'MTU'.ljust(mtu_width)}")
-print(f"{'-'*(dn_width-1)} {'-'*(des_width-1)} {'-'*(speed_width-1)} {'-'*(mtu_width-1)}")
+for head in headers:
+	print(head.ljust(headers[head]-1), end=' ')
+print()
+for sep in headers:
+	print('-'*(headers[sep]-1), end=' ')
+print()
+
 
 
 for src in source:
 	data_res = src["l1PhysIf"]["attributes"]
-	dn = data_res["dn"].ljust(dn_width)
-	des = data_res['descr'].ljust(des_width)
-	speed = data_res['speed'].ljust(speed_width)
-	mtu = data_res['mtu'].ljust(mtu_width)
+	dn = data_res["dn"].ljust(headers['DN'])
+	des = data_res['descr'].ljust(headers['Description'])
+	speed = data_res['speed'].ljust(headers['Speed'])
+	mtu = data_res['mtu'].ljust(headers['MTU'])
 	print(dn, des, speed, mtu)
